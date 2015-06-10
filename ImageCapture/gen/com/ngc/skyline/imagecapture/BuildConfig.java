a8af260bf00e3bad867291263055064de168bed2
Jeremy <Jeremy.Welch@ngc.com>
2015-06-10 19:51:19 -0400

    Initial Project Commit
    Committing all files provided.  This includes binary files and png which
    may be removed if unneeded.

---
+++ ImageCapture/gen/com/ngc/skyline/imagecapture/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ngc.skyline.imagecapture;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ImageCaptureTest/gen/com/ngc/skyline/imagecapturetest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ngc.skyline.imagecapturetest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
