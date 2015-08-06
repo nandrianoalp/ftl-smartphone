8b16799e0f72c992f152a842df4eea82698f60dc
Ivan <ivan.caceres@gmail.com>
2015-08-06 13:11:30 -0700

    changses made to gradle build I believe so that program would run on my galaxy S4. Looks like the picture taking mechanisms are still working but the orientation / position numbers are not being updated.

--- FlightCapture_AS/app/build/generated/source/r/debug/com/example/flightcapture/R.java
+++ FlightCapture_AS/app/build/generated/source/r/debug/com/example/flightcapture/R.java
@@ -14,17 +14,30 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int cancel=0x7f070006;
+        public static final int cancel=0x7f070013;
         public static final int editDist=0x7f070003;
         public static final int editFrames=0x7f070001;
         public static final int editTime=0x7f070002;
-        public static final int menu_settings=0x7f07000a;
+        public static final int menu_settings=0x7f070017;
         public static final int preview=0x7f070000;
-        public static final int readXOrient=0x7f070004;
-        public static final int readYOrient=0x7f070005;
-        public static final int snap=0x7f070009;
-        public static final int start=0x7f070007;
-        public static final int startdist=0x7f070008;
+        public static final int radio0=0x7f070010;
+        public static final int radio1=0x7f070011;
+        public static final int radio2=0x7f070012;
+        public static final int radioGroup1=0x7f07000f;
+        public static final int snap=0x7f070016;
+        public static final int start=0x7f070014;
+        public static final int startdist=0x7f070015;
+        public static final int tableLayout1=0x7f070004;
+        public static final int tableRow1=0x7f070005;
+        public static final int tableRow2=0x7f070008;
+        public static final int tableRow3=0x7f07000b;
+        public static final int tableRow4=0x7f07000e;
+        public static final int textView1=0x7f070006;
+        public static final int textView2=0x7f070009;
+        public static final int textView3=0x7f07000c;
+        public static final int textView4=0x7f070007;
+        public static final int textView5=0x7f07000a;
+        public static final int textView6=0x7f07000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
